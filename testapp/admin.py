57eb007f0f07156799164388db8fcd55120243f0
ZAV1002 <zav1002@gmail.com>
2022-10-13 06:54:14 +0700

    first commit

---
+++ testapp/admin.py
@@ -0,0 +1,22 @@
+from django.contrib import admin
+from .models import Rubric, Article
+# from mptt.admin import MPTTModelAdmin
+from mptt.admin import DraggableMPTTAdmin
+
+
+# admin.site.register(Rubric, MPTTModelAdmin)
+
+admin.site.register(
+    Rubric,
+    DraggableMPTTAdmin,
+    list_display=(
+        'tree_actions',
+        'indented_title',
+        # ...more fields if you feel like it...
+    ),
+    list_display_links=(
+        'indented_title',
+    ),
+)
+
+admin.site.register(Article)
